291c82c87bf816b165ee1c6f08502e195f66f757
CSM3108 <csmahajan31@gmail.com>
2025-04-01 01:43:49 +0530

    Login Signup Done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import {toast} from 'react-hot-toast';
 import {getBaseURL} from '../utils/helperFunctions';
+import { Box, Button, Center, Flex, Heading, Input, InputGroup, InputLeftElement, Link, Stack, Text } from "@chakra-ui/react";
+import { EmailIcon, LockIcon } from "@chakra-ui/icons";
 
 const Login = () => {
     const navigate = useNavigate();
@@ -11,6 +13,10 @@ const Login = () => {
         email : '',
         password : ''
     });
+
+    const navigateToSignup = () =>{
+        navigate('/signup');
+    }
 
     const handleChange = (e) =>{
         const {name, value} = e.target;
@@ -40,19 +46,40 @@ const Login = () => {
     }
 
     return (
-        <div>
-            <form>
-                <label>Email</label>
-                <input
+        <Flex height="100vh">
+      {/* Left Section */}
+      <Box flex={1} bg="green.400" color="white" display="flex" alignItems="center" justifyContent="center" p={12}>
+        <Text fontSize="5xl" fontWeight="bold">
+          One Dashboard, Infinite Possibilities. Sign in to Take Control!
+        </Text>
+      </Box>
+      
+      {/* Right Section */}
+      <Center flex={1} bg="white">
+        <Box p={10} borderWidth={1} borderRadius="lg" borderColor="green.400" boxShadow="md" width="sm">
+          <Heading size="lg" textAlign="center" color="green.600" mb={8}>Login</Heading>
+          
+          <Stack spacing={5}>
+            <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                    <EmailIcon color="gray.400" />
+                </InputLeftElement>
+                <Input 
                     type="text"
                     name="email"
                     value={user.email}
                     onChange={handleChange}
-                    required
+                    required 
+                    placeholder="Email" 
+                    focusBorderColor="green.400" 
                 />
-
-                <label>Password</label>
-                <input
+            </InputGroup>
+            
+            <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                    <LockIcon color="gray.400" />
+                </InputLeftElement>
+                <Input 
                     type="password"
                     name="password"
                     value={user.password}
@@ -60,16 +87,22 @@ const Login = () => {
                     required
                     minLength={8}
                     maxLength={30}
+                    placeholder="Password" 
+                    focusBorderColor="green.400" 
                 />
-
-                <a href="/forgot-password">Forgot Password?</a>
-
-                <button onClick={loginUser}>Login</button>
-            </form>
-            <p>
-                New User? <a href="/signup">Signup</a>
-            </p>
-        </div>
+            </InputGroup>
+            
+            <Link href="./ForgotPassword.jsx" color="gray.500" fontSize="md" textAlign="right">Forget Password ?</Link>
+            
+            <Button onClick={loginUser} colorScheme="orange" width="full" size="lg">Login</Button>
+            
+            <Text fontSize="md" color="gray.500" textAlign="center">
+              New user ? <Link onClick={navigateToSignup} color="green.500">SignUp</Link>
+            </Text>
+          </Stack>
+        </Box>
+      </Center>
+    </Flex>
     );
 };
 
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,82 +1,127 @@
 import React, { useState } from "react";
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
-import {toast} from 'react-hot-toast';
-import {getBaseURL} from '../utils/helperFunctions';
+import { toast } from 'react-hot-toast';
+import { getBaseURL } from '../utils/helperFunctions';
+import { Box, Button, Flex, Heading, Input, InputGroup, InputLeftElement, Link, Stack, Text } from "@chakra-ui/react";
+import { EmailIcon, LockIcon } from "@chakra-ui/icons";
 
 const Signup = () => {
     const navigate = useNavigate();
 
     const [user, setUser] = useState({
-        email : '',
-        name : '',
-        password : ''
+        email: '',
+        name: '',
+        password: '',
+        confirmPassword: ''
     });
 
-    const handleChange = (e) =>{
-        const {name, value} = e.target;
+    const handleChange = (e) => {
+        const { name, value } = e.target;
         setUser({
             ...user,
-            [name] : value
+            [name]: value
         });
-    }
+    };
 
-    const registerUser = (e) =>{
+    const registerUser = (e) => {
         e.preventDefault();
+        if (user.password !== user.confirmPassword) {
+            toast.error("Passwords do not match!");
+            return;
+        }
+
         const toastId = toast.loading('Registering...');
-        axios.post(getBaseURL() + '/auth/signup', user)
-        .then(res =>{
-            if(res.status === 201){
-                toast.success(res.data.message, {id : toastId});
-                setTimeout(()=>{
+        axios.post(getBaseURL() + '/auth/signup', {
+            email: user.email,
+            name: user.name,
+            password: user.password
+        })
+        .then(res => {
+            if (res.status === 201) {
+                toast.success(res.data.message, { id: toastId });
+                setTimeout(() => {
                     navigate('/');
                 }, 1500);
             }
         })
-        .catch(err =>{
+        .catch(err => {
             console.log(err);
-            toast.error(err.response.data.message, {id : toastId});
+            toast.error(err.response?.data?.message || "Something went wrong", { id: toastId });
         });
-    }
+    };
 
     return (
-        <div>
-            <form>
-                <label>Email</label>
-                <input
-                    type="email"
-                    name="email"
-                    value={user.email}
-                    onChange={handleChange}
-                    required
-                />
+        <Flex height="100vh">
+            {/* Left Section */}
+            <Box flex={1} bg="white" display="flex" alignItems="center" justifyContent="center" p={12}>
+                <Box p={10} borderWidth={1} borderRadius="lg" borderColor="green.400" boxShadow="md" width="sm">
+                    <Heading size="lg" textAlign="center" color="green.600" mb={8}>SignUp</Heading>
+                    
+                    <Stack spacing={5}>
+                        <InputGroup>
+                            <InputLeftElement pointerEvents="none">
+                                <EmailIcon color="gray.400" />
+                            </InputLeftElement>
+                            <Input 
+                                type="email"
+                                name="email"
+                                value={user.email}
+                                onChange={handleChange}
+                                required
+                                placeholder="Email" 
+                                focusBorderColor="green.400" 
+                            />
+                        </InputGroup>
 
-                <label>Name</label>
-                <input
-                    type="text"
-                    name="name"
-                    value={user.name}
-                    onChange={handleChange}
-                    required
-                />
+                        <InputGroup>
+                            <InputLeftElement pointerEvents="none">
+                                <LockIcon color="gray.400" />
+                            </InputLeftElement>
+                            <Input 
+                                type="password"
+                                name="password"
+                                value={user.password}
+                                onChange={handleChange}
+                                required
+                                minLength={8}
+                                maxLength={30}
+                                placeholder="Password" 
+                                focusBorderColor="green.400" 
+                            />
+                        </InputGroup>
 
-                <label>Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    value={user.password}
-                    onChange={handleChange}
-                    required
-                    minLength={8}
-                    maxLength={30}
-                />
+                        <InputGroup>
+                            <InputLeftElement pointerEvents="none">
+                                <LockIcon color="gray.400" />
+                            </InputLeftElement>
+                            <Input 
+                                type="password" 
+                                name="confirmPassword"
+                                value={user.confirmPassword}
+                                onChange={handleChange}
+                                required
+                                placeholder="Confirm Password" 
+                                focusBorderColor="green.400" 
+                            />
+                        </InputGroup>
 
-                <button onClick={registerUser}>Signup</button>
-            </form>
-            <p>
-                Already registered? <a href="/">Login</a>
-            </p>
-        </div>
+                        <Button onClick={registerUser} colorScheme="orange" width="full" size="lg">SignUp</Button>
+
+                        <Text fontSize="md" color="gray.500" textAlign="center">
+                            Already a User? <Link onClick={() => navigate('/')} color="green.500">Login</Link>
+                        </Text>
+                    </Stack>
+                </Box>
+            </Box>
+
+            {/* Right Section */}
+            <Box flex={1} bg="green.400" display="flex" alignItems="center" justifyContent="center" p={12}>
+                <Text fontSize="5xl" fontWeight="bold" color="white" textAlign="center">
+                    All Your Socials, One Dashboard. Join Now!
+                </Text>
+            </Box>
+        </Flex>
     );
 };
 
